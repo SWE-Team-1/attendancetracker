c88c46609031a477b408c74cab65697df1f6b06b
Ryan Delong <rdelong@unb.ca>
2020-10-11 14:21:08 -0300

    Adding to Login.js and creating login.css
    This commit is adding an early version of login.css and some more changes to Login.js.

--- attendance_tracker_app/src/Login.js
+++ attendance_tracker_app/src/Login.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import './App.css'
-import './index.css'
-
+import './login.css'
 
 class Login extends React.Component {
   constructor (props) {
@@ -25,19 +23,24 @@ class Login extends React.Component {
   render () {
     return (
       <div className='Login'>
-        <div className='App-header'>
-          <h1>Login</h1>
-          <div />
-          <input>Email</input>
-          <div />
-          <input type="password" placeholder="Password"/>
-          <div />
-          <h3>{this.state.error}</h3>
-          <button onClick={() => this.verifyLogin('test credentials', 'unsecure-password')}>Login</button>
-          <div />
-          <button onClick={() => this.verifyLogin('prof', 'unsecure-password')}>Login as prof</button>
-          <div />
-          <button onClick={() => this.verifyLogin('test credentials', 'not unsecure-password')}>Login Error</button>
+        <div className='box1'>
+        <div className='box2'>
+            <h2>Picture goes here</h2>
+          </div>
+          <div className='box3'>
+            <h1>Login</h1>
+            <div />
+            <input type="email" placeholder="Email"/>
+            <div />
+            <input type="password" placeholder="Password"/>
+            <div />
+            <h3>{this.state.error}</h3>
+            <button onClick={() => this.verifyLogin('test credentials', 'unsecure-password')}>Login</button>
+            <div />
+            <button onClick={() => this.verifyLogin('prof', 'unsecure-password')}>Login as prof</button>
+            <div />
+            <button onClick={() => this.verifyLogin('test credentials', 'not unsecure-password')}>Login Error</button>
+          </div>
         </div>
       </div>
     )
